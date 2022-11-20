f68af2e7c6afc8c883098d33a23996a4a215d709
Blair <hugorosa123@gmail.com>
2022-11-20 23:16:38 +1300

    first update
    Signed-off-by: Blair <hugorosa123@gmail.com>

--- src/com/blair/launchbot/Main.java
+++ src/com/blair/launchbot/Main.java
@@ -4,18 +4,34 @@ import net.dean.jraw.Version;
 import net.dean.jraw.http.NetworkAdapter;
 import net.dean.jraw.http.OkHttpNetworkAdapter;
 import net.dean.jraw.http.UserAgent;
-import net.dean.jraw.models.Listing;
-import net.dean.jraw.models.SubredditSort;
-import net.dean.jraw.models.Submission;
-import net.dean.jraw.models.TimePeriod;
+import net.dean.jraw.models.*;
 import net.dean.jraw.oauth.Credentials;
 import net.dean.jraw.oauth.OAuthHelper;
+import net.dean.jraw.pagination.BarebonesPaginator;
 import net.dean.jraw.pagination.DefaultPaginator;
 import net.dean.jraw.pagination.Paginator;
+import net.dean.jraw.references.SubredditReference;
+
+import java.util.Iterator;
+
 public class Main {
     UserAgent userAgent = new UserAgent("bot", "com.blair.launchbot", "v0.1", "LaunchBot");
     //RedditClient reddit = OAuthHelper.automatic(networkAdapter, scriptOrUserlessCredentials);
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+
+    // Create our credentials
+    Credentials credentials = Credentials.script("LaunchBot", "reHug0rosa",
+            "2qYkmqxUOTiRi_biL1wmyA", "NQaMTaKdD78EHQevzB-UIGrEtNV0Kw");
+
+    // This is what really sends HTTP requests
+    NetworkAdapter adapter = new OkHttpNetworkAdapter(userAgent);
+
+    // Authenticate and get a RedditClient instance
+    RedditClient reddit = OAuthHelper.automatic(adapter, credentials);
+    SubredditReference subreddit = reddit.subreddit("rocketLeague");
+
+    // BarebonesPaginator<Comment> paginator = reddit.latestComments(subreddit.toString()).build();
+    DefaultPaginator<Submission> paginator = reddit.subreddits("Rocketleb", "rkwb").posts().timePeriod(TimePeriod.DAY).build();
+
+
+
 }
